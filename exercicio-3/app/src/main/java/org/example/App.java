d5fe9ad87ca78a7903ff0ae8d5fef50645523489
bobby-coder-dev <bobi77713@gmail.com>
2025-04-05 15:09:46 -0300

    Finalizações  2 e 3 e grandes partes dos objetivos do 4 exercicio realizado

--- exercicio-3/app/src/main/java/org/example/App.java
+++ exercicio-3/app/src/main/java/org/example/App.java
@@ -65,6 +65,7 @@ public class App {
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
                 System.out.print(tabuleiro[i][j]);
+                System.out.print(" ");
             }
             System.out.print("\n");
         }
